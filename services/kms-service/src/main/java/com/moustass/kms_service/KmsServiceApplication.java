90a6f6d67fceb915b3aeeb89b957faedbfee34b3
Tsiory <tsioryrobson@gmail.com>
2026-01-06 00:08:00 +0300

    chore: init frontend and Spring Boot microservices

---
+++ services/kms-service/src/main/java/com/moustass/kms_service/KmsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.moustass.kms_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmsServiceApplication.class, args);
+	}
+
+}
